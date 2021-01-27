e6a467727212cb63a6128ffb0f82c19c129cb896
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2021-01-27 17:01:35 +0900

    Updates

---
+++ precache-manifest.908a0b3a6c810113f4c0b1807f4f5724.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a83804b11417c39ee674ea76e5453b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ae247997b3d0252c976",
+    "url": "/portfolio/static/css/main.98cdfb12.chunk.css"
+  },
+  {
+    "revision": "cb6b18c498298642a172",
+    "url": "/portfolio/static/js/2.53bcdeaf.chunk.js"
+  },
+  {
+    "revision": "4a00a79d20439408526444321fa64abb",
+    "url": "/portfolio/static/js/2.53bcdeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae247997b3d0252c976",
+    "url": "/portfolio/static/js/main.e83b7683.chunk.js"
+  },
+  {
+    "revision": "83f698d4fc7f12bbc12e",
+    "url": "/portfolio/static/js/runtime-main.828378b2.js"
+  },
+  {
+    "revision": "6c6adaafaa1c77dbeb9bd8e37a3f7345",
+    "url": "/portfolio/static/media/brochure01.6c6adaaf.png"
+  },
+  {
+    "revision": "38b44ed4c46a2726b51c03dcf61a40a4",
+    "url": "/portfolio/static/media/brochure02.38b44ed4.png"
+  },
+  {
+    "revision": "4070b7099c1bc0c94b8ca8b211abeb1b",
+    "url": "/portfolio/static/media/calendar01.4070b709.png"
+  },
+  {
+    "revision": "2b1894759f49a61a4d610c44a9dc3e49",
+    "url": "/portfolio/static/media/calling_card01.2b189475.png"
+  },
+  {
+    "revision": "d3043cb9ba6f3ac9684475a993e8abeb",
+    "url": "/portfolio/static/media/catalog01.d3043cb9.png"
+  },
+  {
+    "revision": "6369856654a232e43102da5657bec467",
+    "url": "/portfolio/static/media/cover_of_magazine01.63698566.png"
+  },
+  {
+    "revision": "dd2623432fe1155a47df5953a8e18cab",
+    "url": "/portfolio/static/media/detail_page01.dd262343.png"
+  },
+  {
+    "revision": "f4cb2207ac14d7b72fdcf882d377858c",
+    "url": "/portfolio/static/media/detail_page02.f4cb2207.png"
+  },
+  {
+    "revision": "a820767d7f54e1e8a714a65b63d422ff",
+    "url": "/portfolio/static/media/event_page01.a820767d.png"
+  },
+  {
+    "revision": "7e484184e3a3bd571af0a8ce73b97d90",
+    "url": "/portfolio/static/media/event_page02.7e484184.png"
+  },
+  {
+    "revision": "1d993d105fe6d9fdfd38605f53e6046d",
+    "url": "/portfolio/static/media/event_page03.1d993d10.png"
+  },
+  {
+    "revision": "c5ba10d73d109919e9c24bc6f56f5581",
+    "url": "/portfolio/static/media/home_page01.c5ba10d7.png"
+  },
+  {
+    "revision": "311e4451921d15a80c529d944908818c",
+    "url": "/portfolio/static/media/main_page01.311e4451.png"
+  },
+  {
+    "revision": "90e8fe0770738fb471601b874d073fb5",
+    "url": "/portfolio/static/media/medical_ad_banner01.90e8fe07.png"
+  },
+  {
+    "revision": "4a7d3d064361b9ad65404fe12ab6205e",
+    "url": "/portfolio/static/media/mobile_app01.4a7d3d06.png"
+  },
+  {
+    "revision": "315b033c28c89d6b526ce7fc27e081ea",
+    "url": "/portfolio/static/media/outdoor_advertising01.315b033c.png"
+  },
+  {
+    "revision": "2ce7a09bd8331185da3e8ac9062ace95",
+    "url": "/portfolio/static/media/placard_banner01.2ce7a09b.png"
+  },
+  {
+    "revision": "067f0889f2beece6b38e97823d1c9510",
+    "url": "/portfolio/static/media/platform_banner01.067f0889.png"
+  },
+  {
+    "revision": "a6324c08f9570583685567655fef9b64",
+    "url": "/portfolio/static/media/platform_banner02.a6324c08.png"
+  },
+  {
+    "revision": "66775f754943ea0e6088813f31e7000e",
+    "url": "/portfolio/static/media/popup_banner01.66775f75.png"
+  },
+  {
+    "revision": "75fa5b12ed41234ee9d46953777c607e",
+    "url": "/portfolio/static/media/profile_sh.75fa5b12.jpg"
+  },
+  {
+    "revision": "b68761ba367bb29a54c605df446089b1",
+    "url": "/portfolio/static/media/promotion_banner01.b68761ba.png"
+  },
+  {
+    "revision": "829bea1d7f7df5bc9e7116cffe3ea14f",
+    "url": "/portfolio/static/media/promotion_banner02.829bea1d.png"
+  },
+  {
+    "revision": "a60ebd530e81968931263431cf7da5d6",
+    "url": "/portfolio/static/media/promotion_banner03.a60ebd53.png"
+  },
+  {
+    "revision": "18f3d80600ce7d36f45455dd4f9f520b",
+    "url": "/portfolio/static/media/screen_saver01.18f3d806.png"
+  },
+  {
+    "revision": "54ab69f3d6c00c457ec5ea4265d47093",
+    "url": "/portfolio/static/media/sheet_paper01.54ab69f3.png"
+  },
+  {
+    "revision": "f465239d666976241f05145407dce131",
+    "url": "/portfolio/static/media/sns_contents_banner03.f465239d.png"
+  },
+  {
+    "revision": "e98c2758eaa786b7094a37976e0f4083",
+    "url": "/portfolio/static/media/space_of_a_magazine01.e98c2758.png"
+  },
+  {
+    "revision": "3a13ebe893e8d521debb9320a942aa0e",
+    "url": "/portfolio/static/media/trophy01.3a13ebe8.png"
+  },
+  {
+    "revision": "ba379efae387200dd6e7b6330c79cf52",
+    "url": "/portfolio/static/media/video_editing01.ba379efa.png"
+  },
+  {
+    "revision": "f67f27a168e249e0adb53cbeefe92aa4",
+    "url": "/portfolio/static/media/web_banner01.f67f27a1.png"
+  },
+  {
+    "revision": "7a3d860e4b3d933157457ef64e8f0e3d",
+    "url": "/portfolio/static/media/wide_thumbnail01.7a3d860e.png"
+  },
+  {
+    "revision": "8b76abb531dd6dfcc39e00e5ca349cd6",
+    "url": "/portfolio/static/media/x_banner01.8b76abb5.png"
+  },
+  {
+    "revision": "19cf9b0260fc0b5ba80f59a41cba9a1a",
+    "url": "/portfolio/static/media/x_banner02.19cf9b02.png"
+  }
+]);
