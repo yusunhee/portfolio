90771bc9610b37316082434fbec1682a60ca927b
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2021-01-29 15:01:10 +0900

    UI수정 / 포트폴리오 이미지 교체 및 내용수정

--- src/components/PortfolioModal/PortfolioModal.js
+++ src/components/PortfolioModal/PortfolioModal.js
@@ -17,25 +17,24 @@ class PortfolioModal extends Component {
     return (
       <div>
         <Modal contentClassName="custom-modal" isOpen={show} toggle={toggle}>
-          {/* <ModalHeader toggle={toggle}>{portfolioInfo.title}</ModalHeader> */}
-          <ModalBody>
+          <ModalBody className={portfolioInfo.fontColor}>
             <img src={portfolioInfo.image} width="100%" height="auto" />
-            <i className="close-btn" onClick={toggle}>
+            <span className="close-btn" onClick={toggle}>
               X
-            </i>
+            </span>
             <div className="sub-title-container">{portfolioInfo.subTitle}</div>
             <div className="title-container">{portfolioInfo.title}</div>
             <div className="contents-container">{contentsComp}</div>
             <div className="period-container">
-              <div className="container-title">PERIOD</div>
+              <div className="container-title">작업기간</div>
               <div className="container-contents">{portfolioInfo.period}</div>
             </div>
             <div className="work-container">
-              <div className="container-title">WORK</div>
+              <div className="container-title">기여도</div>
               <div className="container-contents">{portfolioInfo.work}</div>
             </div>
             <div className="tool-container">
-              <div className="container-title">TOOL</div>
+              <div className="container-title">작업 툴</div>
               <div className="container-contents">{portfolioInfo.tool}</div>
             </div>
           </ModalBody>
