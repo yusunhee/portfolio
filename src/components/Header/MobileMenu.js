86c60f1c34bb3aa3d5cc5c8700cf5d4a48b01977
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2021-02-14 00:36:34 +0900

    UI수정 / 모바일 메뉴 내용 및 디자인 수정

--- src/components/Header/MobileMenu.js
+++ src/components/Header/MobileMenu.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 class MobileMenu extends Component {
   render() {
     return (
       <div className="mobile-menu-container">
-        <input type="checkbox" id="menuicon"></input>
+        <input type="checkbox" id="menuicon" />
         <label for="menuicon">
-          <span></span>
-          <span></span>
-          <span></span>
+          <span />
+          <span />
+          <span />
         </label>
-        <div class="sidebar"></div>
+        <div className="sidebar">
+          <ReactFullpage
+            render={({ state, fullpageApi }) => {
+              return (
+                <div className="side-menu-container">
+                  <div
+                    className="menu-text"
+                    onClick={() => fullpageApi.moveTo(1, 0)}
+                  >
+                    INTRO
+                  </div>
+                  <div
+                    className="menu-text"
+                    onClick={() => fullpageApi.moveTo(2, 0)}
+                  >
+                    PROFILE
+                  </div>
+                  <div
+                    className="menu-text"
+                    onClick={() => fullpageApi.moveTo(3, 0)}
+                  >
+                    CAREER
+                  </div>
+                  <div
+                    className="menu-text"
+                    onClick={() => fullpageApi.moveTo(4, 0)}
+                  >
+                    PORTFOLIO
+                  </div>
+                  <div
+                    className="menu-text"
+                    onClick={() => fullpageApi.moveTo(5, 0)}
+                  >
+                    CONTACT
+                  </div>
+                </div>
+              );
+            }}
+          />
+        </div>
       </div>
     );
   }
