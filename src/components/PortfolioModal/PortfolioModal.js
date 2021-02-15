4f26250896aa1c0da2d2d4bb6fd1bca43f9c1735
YUSUNHEE\YUSUNHEE <yusunhee_@naver.com>
2021-02-15 17:03:34 +0900

    UI수정 / 포트폴리오 상세보기화면에서 반응형일때 코드 추가

--- src/components/PortfolioModal/PortfolioModal.js
+++ src/components/PortfolioModal/PortfolioModal.js
@@ -25,25 +25,49 @@ class PortfolioModal extends Component {
     return (
       <div>
         <Modal contentClassName="custom-modal" isOpen={show} toggle={toggle}>
+          <ModalHeader toggle={toggle}>{portfolioInfo.title}</ModalHeader>
           <ModalBody className={portfolioInfo.fontColor}>
-            <img src={portfolioInfo.image} width="100%" height="auto" />
-            <span className="close-btn" onClick={toggle}>
-              X
-            </span>
-            <div className="sub-title-container">{portfolioInfo.subTitle}</div>
-            <div className="title-container">{portfolioInfo.title}</div>
-            <div className="contents-container">{contentsComp}</div>
-            <div className="period-container">
-              <div className="container-title">작업기간</div>
-              <div className="container-contents">{portfolioInfo.period}</div>
+            <div className="web-display">
+              <img src={portfolioInfo.image} width="100%" height="auto" />
+              <span className="close-btn" onClick={toggle}>
+                X
+              </span>
+              <div className="sub-title-container">
+                {portfolioInfo.subTitle}
+              </div>
+              <div className="title-container">{portfolioInfo.title}</div>
+              <div className="contents-container">{contentsComp}</div>
+              <div className="period-container">
+                <div className="container-title">작업기간</div>
+                <div className="container-contents">{portfolioInfo.period}</div>
+              </div>
+              <div className="work-container">
+                <div className="container-title">기여도</div>
+                <div className="container-contents">{portfolioInfo.work}</div>
+              </div>
+              <div className="tool-container">
+                <div className="container-title">작업 툴</div>
+                <div className="container-contents">{portfolioInfo.tool}</div>
+              </div>
             </div>
-            <div className="work-container">
-              <div className="container-title">기여도</div>
-              <div className="container-contents">{portfolioInfo.work}</div>
-            </div>
-            <div className="tool-container">
-              <div className="container-title">작업 툴</div>
-              <div className="container-contents">{portfolioInfo.tool}</div>
+            <div className="mobile-display">
+              <img src={portfolioInfo.image} width="100%" height="auto" />
+              <div className="sub-title-container">
+                {portfolioInfo.subTitle}
+              </div>
+              <div className="period-container">
+                <div className="container-title">작업기간</div>
+                <div className="container-contents">{portfolioInfo.period}</div>
+              </div>
+              <div className="work-container">
+                <div className="container-title">기여도</div>
+                <div className="container-contents">{portfolioInfo.work}</div>
+              </div>
+              <div className="tool-container">
+                <div className="container-title">작업 툴</div>
+                <div className="container-contents">{portfolioInfo.tool}</div>
+              </div>
+              <div className="contents-container">{contentsComp}</div>
             </div>
           </ModalBody>
         </Modal>
